e22ce4f5045b8cd7212d1af13ceed42bfd8cdac2
zhang_glb <268048375_glb@qq.com>
2021-09-30 01:31:33 +0800

    hot-fix commit2

--- src/main/java/com/javasm/git/gitTest.java
+++ src/main/java/com/javasm/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("helloaaaaaaaaaaaaa");
         System.out.println("helloaaasd");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
