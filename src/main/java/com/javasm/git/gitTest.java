c5c6a0779fc865e8f2aeb53833a7b5ee4f981bc9
zhang_glb <268048375_glb@qq.com>
2021-09-30 02:01:15 +0800

    hot-fix commit3

--- src/main/java/com/javasm/git/gitTest.java
+++ src/main/java/com/javasm/git/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("hot-fix2");
+        System.out.println("hot-fix3");
     }
 }
